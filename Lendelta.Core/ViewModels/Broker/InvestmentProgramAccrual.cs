55c9405b37b8798a00ae616a04cf66fa565ce58d
LENDELTA <38677091+LENDELTA@users.noreply.github.com>
2018-06-01 16:34:09 +0300

    Update InvestmentProgramAccrual.cs

--- Lendelta.Core/ViewModels/Broker/InvestmentProgramAccrual.cs
+++ Lendelta.Core/ViewModels/Broker/InvestmentProgramAccrual.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GenesisVision.Core.ViewModels.Broker
+namespace LENDELTA.Core.ViewModels.Broker
 {
     public class InvestorAmount
     {
