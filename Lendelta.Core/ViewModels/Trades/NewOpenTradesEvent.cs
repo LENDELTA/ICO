bae27a36a6b7bec40a9695bb585ab8ab8a5facc5
LENDELTA <38677091+LENDELTA@users.noreply.github.com>
2018-06-01 17:13:15 +0300

    Update NewOpenTradesEvent.cs

--- Lendelta.Core/ViewModels/Trades/NewOpenTradesEvent.cs
+++ Lendelta.Core/ViewModels/Trades/NewOpenTradesEvent.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace GenesisVision.Core.ViewModels.Trades
+namespace LENDELTA.Core.ViewModels.Trades
 {
     public class NewOpenTradesEvent
     {
