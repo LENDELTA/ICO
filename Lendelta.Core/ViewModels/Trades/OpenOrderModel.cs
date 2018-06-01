7bb27baab924cb9e07668738a1db61d12da7e916
LENDELTA <38677091+LENDELTA@users.noreply.github.com>
2018-06-01 17:13:56 +0300

    Update OpenOrderModel.cs

--- Lendelta.Core/ViewModels/Trades/OpenOrderModel.cs
+++ Lendelta.Core/ViewModels/Trades/OpenOrderModel.cs
@@ -1,9 +1,9 @@
-﻿using GenesisVision.DataModel.Enums;
+﻿using LENDELTA.DataModel.Enums;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using System;
 
-namespace GenesisVision.Core.ViewModels.Trades
+namespace LENDELTA.Core.ViewModels.Trades
 {
     public class OpenOrderModel
     {
